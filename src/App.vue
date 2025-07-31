9d6cf214a5abd56ff4ec0055b152b1ec775d6790
Anders K <and3k5@users.noreply.github.com>
2025-07-31 23:19:48 +0200

    Update site a bit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const r = ref(180);
+
+setInterval(() => (r.value += -360 + Math.random() * (360 * 2)), 333);
+</script>
+
+<template>
+    Yo.<br />
+    <span class="smiley" :style="{ transform: 'rotate(' + r + 'deg)' }">🤪</span>
+</template>
+
+<style scoped>
+.smiley {
+    display: inline-block;
+    transition: transform 400ms linear;
+}
+</style>
